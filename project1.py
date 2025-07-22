1b7fb1e4fbf351e75695afa188822b1eaa9dd9f5
nhokcherry <baosuper456@gmail.com>
2025-07-22 11:42:04 +0700

    Add more comments (modify)

--- project1.py
+++ project1.py
@@ -1,5 +1,5 @@
 import random
-test_board = ['#','X','O','X','O','X','O','X','O','X'] # board to test function
+test_board = ['#','X','O','X','O','X','O','X','O','X'] # board to test game function 
 
 #### Display board ####
 
